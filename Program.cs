0c5b1fd8920c6e1e8751cd3ea3924410e5c5b99d
Roy <ningjiyu@gmail.com>
2015-07-28 14:48:13 +1000

    TextFieldParser Method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 using Microsoft.VisualBasic.FileIO;
+using System.Configuration;
+
 
 namespace CSVExtracter
 {
@@ -17,79 +19,45 @@ namespace CSVExtracter
             string SourceFilePath = args[0];
             string TargetFilePath = args[1];
 
+            List<string> ResultList = TextFieldParserMethod(SourceFilePath);
+            ExportCSV(TargetFilePath, ResultList);
+        }
 
-
-            Stopwatch sw = Stopwatch.StartNew();
-            //using (TextFieldParser parser = new TextFieldParser(SourceFilePath))
-            //{
-            //    parser.TextFieldType = FieldType.Delimited;
-            //    parser.SetDelimiters(",");           
-
-            //    string[] header = parser.ReadFields();
-            //    int physicalDeliveryOfficeNameIndex = Array.IndexOf(header, "physicalDeliveryOfficeName");
-            //    int nameIndex = Array.IndexOf(header, "name");
-            //    int titleIndex = Array.IndexOf(header, "title");
-                
-            //    string[] CurrentRow;
-            //    List<string> ResultList = new List<string>();
-
-            //    while (!parser.EndOfData)
-            //    {
-            //        //Processing row
-            //        string[] fields = parser.ReadFields();
-            //        CurrentRow = new string[3] { fields[physicalDeliveryOfficeNameIndex], fields[nameIndex], fields[titleIndex] };
-            //        ResultList.Add(string.Join(",", CurrentRow));
-            //    }
-            //}
-
-            // Read csv
-            List<string> SourceFileLines = File.ReadLines(SourceFilePath).ToList();
-            sw.Stop();
-            TimeSpan elapsedTime = sw.Elapsed;
-            Console.WriteLine("Time to load source lines: " + elapsedTime);
-
-
-
-
-            
-
-
-
-            sw = Stopwatch.StartNew();
-
-            // Find Index
-            string[] header = SourceFileLines[0].Split(',').ToArray();
-            int physicalDeliveryOfficeNameIndex = Array.IndexOf(header, "physicalDeliveryOfficeName");
-            int nameIndex = Array.IndexOf(header, "name");
-            int titleIndex = Array.IndexOf(header, "title");
-
-            sw.Stop();
-            elapsedTime = sw.Elapsed;
-            Console.WriteLine("Time to find index: " + elapsedTime);
-
-            sw = Stopwatch.StartNew();
-
-            // Extract Columns
+        // TextFieldParser Method 
+        private static List<string> TextFieldParserMethod(string SourceFilePath)
+        {
             List<string> ResultList = new List<string>();
-            string[] CurrentRow;
-            foreach (string line in SourceFileLines)
+
+            // Parser
+            
+            using (TextFieldParser parser = new TextFieldParser(SourceFilePath))
             {
-                CurrentRow = Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
-                if (!string.IsNullOrEmpty(CurrentRow[physicalDeliveryOfficeNameIndex]))
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+
+                // Find Index
+                string[] header = parser.ReadFields();
+                int physicalDeliveryOfficeNameIndex = Array.IndexOf(header, "physicalDeliveryOfficeName");
+                int nameIndex = Array.IndexOf(header, "name");
+                int titleIndex = Array.IndexOf(header, "title");
+
+                string[] CurrentRow;
+               
+                while (!parser.EndOfData)
                 {
-                    CurrentRow = new string[3] { CurrentRow[physicalDeliveryOfficeNameIndex], CurrentRow[nameIndex], CurrentRow[titleIndex] };
+                    //Processing row
+                    string[] fields = parser.ReadFields();
+                    CurrentRow = new string[3] { fields[physicalDeliveryOfficeNameIndex], fields[nameIndex], fields[titleIndex] };
+                    if (!string.IsNullOrEmpty(CurrentRow[0]))
                     ResultList.Add(string.Join(",", CurrentRow));
                 }
             }
-
-            sw.Stop();
-            elapsedTime = sw.Elapsed;
-            Console.WriteLine("Time to extract columns: " + elapsedTime);
-
-
-            sw = Stopwatch.StartNew();
-
-            // Export CSV
+            return ResultList;
+        }
+        
+        // Export CSV Method
+        private static void ExportCSV(string TargetFilePath, List<string> ResultList)
+        {
             using (System.IO.StreamWriter file = new System.IO.StreamWriter(TargetFilePath))
             {
                 foreach (string line in ResultList)
@@ -97,11 +65,6 @@ namespace CSVExtracter
                     file.WriteLine(line);
                 }
             }
-
-            sw.Stop();
-            elapsedTime = sw.Elapsed;
-            Console.WriteLine("Time to exporet csv: " + elapsedTime);
-            Console.ReadKey();
         }
     }
 }
